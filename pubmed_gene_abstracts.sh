017ef41b4e9a0677d6425d2271630a09383d9d38
mvelinder <mvelinder@gmail.com>
2020-04-20 09:29:52 -0600

    fix dir pointing

--- pubmed_gene_abstracts.sh
+++ pubmed_gene_abstracts.sh
@@ -1,7 +1,8 @@
 INPUT=$1
+PDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-awk -v INPUT=$INPUT '$3 == INPUT' <(zcat data/Homo_sapiens.gene_info) \
+awk -v INPUT=$INPUT '$3 == INPUT' <(zcat $PDIR/data/Homo_sapiens.gene_info) \
 	| cut -f 2 \
-	| while read line; do awk -v LINE=$line '$1 == "9606" && $2 == LINE' <(zcat data/gene2pubmed); done \
+	| while read line; do awk -v LINE=$line '$1 == "9606" && $2 == LINE' <(zcat $PDIR/data/gene2pubmed); done \
 	| cut -f 3 \
 	| while read line; do curl -s https://www.ncbi.nlm.nih.gov/pubmed/$line | grep rprt_all | awk '{OFS="\n"} {split($0,a,"<h1>"); split(a[2],b,"</h1>"); split($0,c,"<p>"); split(c[2],d,"</p>"); print "Title: " b[1], "Abstract: " d[1] "\n"}'; done
