ca80d77e0cb837c4e8799d7722186135b513081a
mvelinder <mvelinder@gmail.com>
2020-06-16 13:17:45 -0600

    changed for new pubmed website

--- gene_abstracts.sh
+++ gene_abstracts.sh
@@ -15,4 +15,4 @@ awk -v INPUT=$INPUT '$3 == INPUT' <(zcat $PDIR/data/Homo_sapiens.gene_info) \
 	| cut -f 2 \
 	| while read line; do awk -v LINE=$line '$1 == "9606" && $2 == LINE' <(zcat $PDIR/data/gene2pubmed); done \
 	| cut -f 3 \
-	| while read line; do curl -s https://www.ncbi.nlm.nih.gov/pubmed/$line | grep rprt_all | awk '{OFS="\n"} {split($0,a,"<h1>"); split(a[2],b,"</h1>"); split($0,c,"<p>"); split(c[2],d,"</p>"); print "Title: " b[1], "Abstract: " d[1] "\n"}'; done
+	| while read line; do bash pmid_abstract.sh $line; done
--- pmid_abstract.sh
+++ pmid_abstract.sh
@@ -1,9 +1,8 @@
-### Usage
-# bash pmid_abstract.sh $PMID
-# where $PMID is the PubMed ID, for example 25741868 for the ACMG guidelines paper at https://www.ncbi.nlm.nih.gov/pubmed/25741868
-# Example: bash pmid_abstract.sh 25741868
-
-
+#! /bin/bash
 
 PMID=$1
-curl -s https://www.ncbi.nlm.nih.gov/pubmed/$PMID | grep rprt_all | awk '{OFS="\n"} {split($0,a,"<h1>"); split(a[2],b,"</h1>"); split($0,c,"<p>"); split(c[2],d,"</p>"); print "Title: " b[1], "Abstract: " d[1] "\n"}'
+
+curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep -A 7 "heading-title" | tail -n+6 | tail -n 1 | sed 's/  //g'
+curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep -A 10 "abstract-content" | tail -n 1 | sed 's/      //g'
+echo " "
+#curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep rprt_all | awk '{OFS="\n"} {split($0,a,"<h1>"); split(a[2],b,"</h1>"); split($0,c,"<p>"); split(c[2],d,"</p>"); print "Title: " b[1], "Abstract: " d[1] "\n"}'
