b7c83ba39f0e8270f4abdbbec47cbaadab871783
mvelinder <mvelinder@gmail.com>
2021-01-20 12:34:45 -0700

    fix for pubmed abstracts by gene and id

--- pubmed_gene_abstracts.sh
+++ pubmed_gene_abstracts.sh
@@ -6,4 +6,4 @@ awk -v INPUT=$INPUT '$3 == INPUT' /scratch/ucgd/lustre-work/marth/u0691312/refer
 	| cut -f 2 \
 	| while read line; do awk -v LINE=$line '$1 == "9606" && $2 == LINE' /scratch/ucgd/lustre-work/marth/u0691312/reference/gene2pubmed; done \
 	| cut -f 3 \
-	| while read line; do echo "https://pubmed.ncbi.nlm.nih.gov/$line"; bash /uufs/chpc.utah.edu/common/HIPAA/u0691312/scripts/pubmed_id_abstract.sh $line; done
+	| while read line; do echo $INPUT "https://pubmed.ncbi.nlm.nih.gov/$line"; bash /uufs/chpc.utah.edu/common/HIPAA/u0691312/scripts/pubmed_id_abstract.sh $line; done
--- pubmed_id_abstract.sh
+++ pubmed_id_abstract.sh
@@ -3,6 +3,6 @@
 PMID=$1
 
 curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep -A 7 "heading-title" | tail -n+6 | tail -n 1 | sed 's/  //g'
-curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep -A 10 "abstract-content" | tail -n 1 | sed 's/      //g'
+curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep -A 10 "abstract-content" | tail -n 2 | head -n 1 | sed 's/      //g'
 echo " "
 #curl -s https://pubmed.ncbi.nlm.nih.gov/$PMID/ | grep rprt_all | awk '{OFS="\n"} {split($0,a,"<h1>"); split(a[2],b,"</h1>"); split($0,c,"<p>"); split(c[2],d,"</p>"); print "Title: " b[1], "Abstract: " d[1] "\n"}'
